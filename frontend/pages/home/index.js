4f54684ee1e4688b811f8afb911f7ec6006c9b5a
Andre Freitas <freitasandre38@gmail.com>
2025-06-19 21:45:22 -0300

    feat: create logout route, fix security errors, set time generator to be reused, create logout script in frontend, set user id in local storage, add register route script

--- frontend/pages/home/index.js
+++ frontend/pages/home/index.js
@@ -48,4 +48,29 @@ document.addEventListener('DOMContentLoaded', async () => {
       console.error('Error getting user information:', err);
     }
   });
+ 
+
+const logoutBtn = document.getElementById('logout-tag');
+logoutBtn.addEventListener('click', async e => {
+  e.preventDefault();
+  await logout();
+  window.location.href = '/pages/initial/index.html';
+});
+
+
+async function logout() {
+  const token = localStorage.getItem('token');
+  const user_id = localStorage.getItem('user_id');
+  const logout = await fetch('/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'token': token,
+    },
+    body: JSON.stringify({ user_id: user_id})
+  });
   
+  if(logout.status === 200)
+    alert("Logout successful");
+
+}
